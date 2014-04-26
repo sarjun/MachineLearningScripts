2a44a1c9e8897840c5c5d697c237c936480c92c0
Arjun Shankar <sarjun93@gmail.com>
2014-04-26 15:05:27 -0400

    Working on add SMOTE to LogisticRegression model

--- LogisticRegression.R
+++ LogisticRegression.R
@@ -1,8 +1,12 @@
 library(rms)
 library(pROC)
+library(DMwR)
+
+# Determine which methods/formula to use
+useSMOTE<-T
 
 # Read in the data and set up train/test sets
-data<-read.table("C:/Users/Arjun/Dropbox/Smash Music/anontfmodel2_R.csv",header=T,sep=",")
+data<-read.table("C:/Users/Arjun/Documents/UVa/Sixth Semester/CS 6316/ResearchProject/DataSet2.csv",header=T,sep=",")
 data<-data[with(data, order(id)), ]
 splitIndex <- trunc(nrow(data)*0.66)
 while(data$id[splitIndex]==data$id[splitIndex+1]) {
@@ -11,6 +15,12 @@ while(data$id[splitIndex]==data$id[splitIndex+1]) {
 trainset <- data[1:splitIndex,]
 testset <- data[(splitIndex+1):nrow(data),]
 attach(trainset)
+
+# Apply preprocessing methods
+if(useSMOTE) {
+  trainset <- 
+}
+
 
 # Build the logistic regression model and calculate ROC curve
 optimal<-lrm(Class~HR+BP_S+BP_M+BP_D+LDS,y=T,x=T)
